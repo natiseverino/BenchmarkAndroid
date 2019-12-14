7a87aa18c5dda8e3f2be0cd21ccf2fdf9d403398
BautistaCRP <bj.carpintero@gmail.com>
2019-12-14 14:26:07 -0300

    Check length of StringBuilder on LogGUI

--- app/src/main/java/edu/benchmarkandroid/utils/LogGUI.java
+++ app/src/main/java/edu/benchmarkandroid/utils/LogGUI.java
@@ -6,6 +6,7 @@ public class LogGUI {
 
     private static TextView logTextView = null;
     private static StringBuilder stringBuilder;
+    private static final int MAX_STRING_BUILDER = 1_000_000;
 
 
     public static void init(TextView logTextView) {
@@ -22,8 +23,19 @@ public class LogGUI {
                 line = lines[lines.length - 1];
 
             stringBuilder.append(line).append("\n");
+            checkStringBuilderLength();
             logTextView.setText(stringBuilder.toString());
         }
 
+    }
+
+
+    private static void checkStringBuilderLength() {
+        if (stringBuilder != null)
+            if (stringBuilder.length() >= MAX_STRING_BUILDER) {
+                stringBuilder = stringBuilder.reverse();
+                stringBuilder.setLength(MAX_STRING_BUILDER / 2);
+                stringBuilder = stringBuilder.reverse();
+            }
     }
 }
